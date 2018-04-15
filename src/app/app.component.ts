111025470ee894a48ae1d3cf8b5af8a13133cbdc
fabianodiassilva <biano_fds@hotmail.com>
2018-04-15 19:06:28 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { PaginaFormularioPage } from '../pages/pagina-formulario/pagina-formulario';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'PaginaFormularioPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
